be4e511eb837dc95df4ee3045c70de2a162cc621
Abdul-Wahaab Gool <Abdul-Wahaab.Gool@vista.co>
2021-12-03 14:33:01 +0200

    be calc

--- BowlingGame.Web/Models/BowlingContestant.cs
+++ BowlingGame.Web/Models/BowlingContestant.cs
@@ -20,7 +20,63 @@ namespace BowlingGame.Web.Models
 
         public int GetScore()
         {
-            throw new NotImplementedException();
+            int totalScore = 0;
+            //calculate score for frames 1-9
+            foreach (var group in ScoringData.Where(x => x.ScoreFrame != 10).GroupBy(x => x.ScoreFrame))
+            {
+                int frameScore = 0;
+                int frameNumber = group.Key;
+                IEnumerable<IScoreRecord> currentFrame = group.AsEnumerable();
+
+                //its a strike
+                if (currentFrame.Count() == 1 && currentFrame.Sum(x => x.Score) == 10)
+                {
+                    IScoreRecord frame = currentFrame.Last();
+                    int index = ScoringData.LastIndexOf(frame);
+                    try
+                    {
+                        frameScore = 10 + ScoringData[index + 1].Score + ScoringData[index + 2].Score;
+                    }
+                    catch
+                    {
+                        try
+                        {
+                            frameScore = 10 + ScoringData[index + 1].Score;
+                        }
+                        catch
+                        {
+                            frameScore = 10;
+                        }
+
+                    }
+
+                }
+                else if (currentFrame.Count() == 2 && currentFrame.Sum(x => x.Score) == 10)
+                {
+                    IScoreRecord frame = currentFrame.Last();
+                    int index = ScoringData.LastIndexOf(frame);
+                    try
+                    {
+                        frameScore = 10 + ScoringData[index + 1].Score;
+                    }
+                    catch
+                    {
+                        frameScore = 10;
+                    }
+
+                }
+                else
+                {
+                    frameScore = currentFrame.Sum(x => x.Score);
+                }
+
+                totalScore += frameScore;
+            }
+
+            //add 10th frame
+            totalScore += ScoringData.Where(x => x.ScoreFrame == 10).Sum(x => x.Score);
+
+            return totalScore;
         }
     }
 }
